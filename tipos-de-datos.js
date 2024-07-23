da72e77985796700786f338e8904ec299c7f055f
DiegoGomez <diegocor4@gmail.com>
2024-07-23 11:15:13 -0300

    tipos de datos

---
+++ tipos-de-datos.js
@@ -0,0 +1,36 @@
+//Primitivos
+
+//String
+let nombre = 'diego'
+
+//Number
+let edad = 45
+
+//Bolean
+let esMayorDeEdad = true
+
+//null
+let noHayValor = null
+
+//undefined
+let noDefinido = undefined
+
+//symbol
+let simboloUnico = Symbol('unico')
+
+//bitint
+let numroGrande = 2n
+
+//COMPLEJOS
+
+//Object
+let carro = {
+    marca: 'tesla',
+    modelo: 'model s'
+}
+
+//array
+let frutas = ['manzana', 'banana', 'pera']
+
+//function
+function saludar (){}
